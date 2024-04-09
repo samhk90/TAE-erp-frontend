3db56b9759a701e8be1888120b33d21f930b8f2a
Amit Das <amitkumarr9168@gmail.com>
2024-04-10 00:45:59 +0530

    new changes made

--- script.js
+++ script.js
@@ -1,74 +1,29 @@
-const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+const number = document.getElementById("num");
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
+const randomNum = 50;
 
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
-});
+let counter = 0;
 
+const time = 2000 / randomNum;
 
-// TOGGLE SIDEBAR
-const menuBar = document.querySelector('#content nav .bx.bx-menu');
-const sidebar = document.getElementById('sidebar');
+setInterval(() => {
+    if (counter === randomNum) {
+        clearInterval();
+        number.innerHTML = randomNum + "%";
+    } else {
+        counter += 1;
+        number.innerHTML = counter + "%";
+    }
+}, time);
 
-menuBar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
-})
+const css = window.document.styleSheets[0];
 
+const stroke = 440 - (440 * (randomNum / 100));
 
-
-
-
-
-
-const searchButton = document.querySelector('#content nav form .form-input button');
-const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
-const searchForm = document.querySelector('#content nav form');
-
-searchButton.addEventListener('click', function (e) {
-	if(window.innerWidth < 576) {
-		e.preventDefault();
-		searchForm.classList.toggle('show');
-		if(searchForm.classList.contains('show')) {
-			searchButtonIcon.classList.replace('bx-search', 'bx-x');
-		} else {
-			searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		}
-	}
-})
-
-
-
-
-
-if(window.innerWidth < 768) {
-	sidebar.classList.add('hide');
-} else if(window.innerWidth > 576) {
-	searchButtonIcon.classList.replace('bx-x', 'bx-search');
-	searchForm.classList.remove('show');
+css.insertRule(`
+@keyframes anim {
+    100% {
+        stroke-dashoffset: ${stroke};
+    }
 }
-
-
-window.addEventListener('resize', function () {
-	if(this.innerWidth > 576) {
-		searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		searchForm.classList.remove('show');
-	}
-})
-
-
-
-const switchMode = document.getElementById('switch-mode');
-
-switchMode.addEventListener('change', function () {
-	if(this.checked) {
-		document.body.classList.add('dark');
-	} else {
-		document.body.classList.remove('dark');
-	}
-})
+`, css.cssRules.length);
